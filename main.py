cbf0970110c0741e10bc666d94a22d2c5638526d
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-06-12 00:11:44 +0300

    Почав писати підтримку БД

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from PyQt5 import QtSql
+
 
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi('ui.ui')
+
 ui.show()
 app.exec()
+
+db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+db.setDatabaseName('autotool.db')
