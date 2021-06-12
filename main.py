9c3f1563af63869f6ec9129cc990b0a6c123cdbb
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-06-13 00:19:15 +0300

    Прописав обробники кнопок

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ from PyQt5 import QtSql
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi('ui.ui')
+ui.setWindowTitle('Autotool v 1.0 06/2021')
+
+def addBtnHandler():
+    print('Add Button pressed')
+
+def delBtnHandler():
+    print('Del Button pressed')
+
+ui.addBtn.clicked.connect(addBtnHandler)
+ui.delBtn.clicked.connect(delBtnHandler)
+
+db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+db.setDatabaseName('autotool.db')
+
+ui.tableWidget.setColumnCount(4)
+ui.tableWidget.setRowCount(4)
+
+
+
+
 
 ui.show()
 app.exec()
 
-db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-db.setDatabaseName('autotool.db')
