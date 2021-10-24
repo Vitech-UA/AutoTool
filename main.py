09f2e33eb413ad398e639496046341c0da426aff
Віктор <74230330+Vitech-UA@users.noreply.github.com>
2021-10-24 10:04:55 +0300

    додав loging

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ from ui import Ui_MainWindow
 from autotool import Autotool
 
 app = QtWidgets.QApplication(sys.argv)
-
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 ui.addBtn.clicked.connect(Autotool.add_btn_handler)
 
+logging.basicConfig(level=logging.INFO)
 
 
 # підгружу поточну дату в DateEdit
+logging.info('Завантажую системну дату')
 date = QDate.currentDate()
 ui.part_date_edit.setDate(date)
 
-# підгружу дані з бази у таблицю
 MainWindow.show()
 
 sys.exit(app.exec_())
