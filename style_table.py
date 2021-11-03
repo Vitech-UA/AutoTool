19106651d5e8e376e7b2f927fa375850e7a7e731
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-11-03 23:10:29 +0200

    Оновив таблицю стилів

---
+++ style_table.py
@@ -0,0 +1,9 @@
+
+class styles:
+
+    table_style = ''' QTableWidget::item {background-color: white;
+        border-style: outset;
+        border-width: 2px; border-radius: 5px; border-color: grey} 
+    
+        border-width: 3px; border-radius: 1px; color: green; border-color: blue}'''
+    line_edit_style = ''' '''
