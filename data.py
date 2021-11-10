6d780f487dd97dc471f28330ba299f7b31ac8e4d
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-11-11 00:14:50 +0200

    У конфігураційному файлі тепер складено всі шляхи до баз даних.

--- data.py
+++ data.py
@@ -13,6 +13,8 @@ class CommonMethods:
         self.program_path = os.path.abspath(os.curdir)
         self.db_folder_name = 'user_data'
         self.db_folder_path = self.program_path + '\\' + self.db_folder_name
+        self.conf_file_name = "config.ini"
+
         if not os.path.exists(self.db_folder_path):
             logging.info("створюю папку {}".format(self.db_folder_path))
             os.mkdir(self.db_folder_path)
@@ -38,6 +40,16 @@ class DataBaseImporter(CommonMethods):
         # Копіюю файл у папку з програмою
         shutil.copy(selected_file_path_name, self.db_folder_path)
         dialogs.Messages.message_import_success()
+        file_name = selected_file_path_name.split('/')
+        print(file_name[-1])
+
+        # Запишу у файл конфігурації інформацію про додану базу даних
+        try:
+            f = open(self.conf_file_name, 'a')
+            f.write(self.db_folder_path + '\\' + file_name[-1] + '\n')
+            f.close()
+        except FileNotFoundError:
+            print("Файлу не існує!!!")
 
 
 class DataBaseExporter(CommonMethods):
