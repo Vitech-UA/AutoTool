5d4ef85abddd08d213be20a20662abb1ecba2e2c
viktorstarovit@gmail.com <viktorstarovit@gmail.com>
2024-01-22 22:29:36 +0200

    init_v2

--- main.py
+++ main.py
@@ -1,40 +1,26 @@
-import logging
-import sys
+import openpyxl as op
+from datetime import datetime
+import json
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import (
-    QToolBar, QAction
-)
+filename = 'fuel_data.xlsx'
+wb = op.load_workbook(filename)
+sheet = wb.active
+rows_cnt = sheet.max_row
+record_dict = {}
 
-from data import DataBaseImporter, DataBaseExporter
-from ui import Ui_MainWindow
+for i in range(6, rows_cnt + 1):
+    if sheet.cell(i, 1).value:
+        date_str = str(sheet.cell(i, 1).value)
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        date_to_append = str(date_obj.date())
 
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(MainWindow)
+        dict_data = {
+            'sum': sheet.cell(i, 2).value,
+            'liter': sheet.cell(i, 3).value,
+            'type': 'LPG' if sheet.cell(i, 5).value == 'Г' else 'Gasoline',
+            'km': sheet.cell(i, 6).value, }
+        dict_to_append = {date_to_append: dict_data}
+        record_dict.update(dict_to_append)
 
-importer = DataBaseImporter()
-
-# Створюю тулбар
-toolbar = QToolBar('my_toolbar')
-MainWindow.addToolBar(toolbar)
-
-# Кнопка імпорта нової бази даних
-import_action = QAction("Імпорт бази даних")
-import_action.setStatusTip("Імпорт вже існуючої бази даних")
-import_action.triggered.connect(importer.import_database)
-toolbar.addAction(import_action)
-
-# Кнопка експорту наявної бази даних
-export_action = QAction("Експорт бази даних")
-export_action.setStatusTip("ЕКспорт вже існуючої бази даних")
-export_action.triggered.connect(DataBaseExporter.export_database)
-toolbar.addAction(export_action)
-
-# Налаштування виведення логів
-logging.basicConfig(level=logging.INFO)
-MainWindow.setFixedSize(1085, 500)
-MainWindow.show()
-
-sys.exit(app.exec_())
+with open('fuel_data.json', 'w') as f:
+    json.dump(record_dict, f)
--- program.py
+++
@@ -1,6 +0,0 @@
-class Program:
-    program_name = "Autotool"
-    program_version = "1.1"
-
-    def __init__(self):
-        pass
